cade965b417ef9bbd4e97b81a0880fc32c358978
Konstantin Bogdanov <konstantin@clickhouse.com>
2024-07-11 05:03:38 +0200

    Cleanup

--- src/Server/HTTPHandlerFactory.cpp
+++ src/Server/HTTPHandlerFactory.cpp
@@ -8,6 +8,7 @@
 #include <Poco/Util/AbstractConfiguration.h>
 
 #include "HTTPHandler.h"
+#include "Server/ACMEClient.h"
 #include "Server/PrometheusMetricsWriter.h"
 #include "StaticRequestHandler.h"
 #include "ReplicasStatusHandler.h"
@@ -251,6 +252,11 @@ void addCommonDefaultHandlersFactory(HTTPRequestHandlerFactoryMain & factory, IS
     binary_handler->allowGetAndHeadRequest();
     factory.addPathToHints("/binary");
     factory.addHandler(binary_handler);
+
+    auto acme_handler = std::make_shared<HandlingRuleHTTPHandlerFactory<ACMERequestHandler>>(server);
+    acme_handler->attachNonStrictPath(ACMEClient::ACME_CHALLENGE_PATH);
+    acme_handler->allowGetAndHeadRequest();
+    factory.addHandler(acme_handler);
 
     auto js_handler = std::make_shared<HandlingRuleHTTPHandlerFactory<JavaScriptWebUIRequestHandler>>(server);
     js_handler->attachNonStrictPath("/js/");
