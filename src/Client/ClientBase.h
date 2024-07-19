402b7ed490113bd690c10f87cf3d4daa0ed51a62
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2024-07-19 12:25:17 +0000

    Fix build

--- src/Client/ClientBase.h
+++ src/Client/ClientBase.h
@@ -9,9 +9,6 @@
 #include <Common/Stopwatch.h>
 #include <Core/ExternalTable.h>
 #include <Core/Settings.h>
-#include <Poco/ConsoleChannel.h>
-#include <Poco/SimpleFileChannel.h>
-#include <Poco/SplitterChannel.h>
 #include <Interpreters/Context.h>
 #include <Parsers/ASTCreateQuery.h>
 #include <Poco/Util/Application.h>
@@ -246,13 +243,6 @@ protected:
     /// since other members can use them.
     SharedContextHolder shared_context;
     ContextMutablePtr global_context;
-
-    LoggerPtr fatal_log;
-    Poco::AutoPtr<Poco::SplitterChannel> fatal_channel_ptr;
-    Poco::AutoPtr<Poco::Channel> fatal_console_channel_ptr;
-    Poco::AutoPtr<Poco::Channel> fatal_file_channel_ptr;
-    Poco::Thread signal_listener_thread;
-    std::unique_ptr<Poco::Runnable> signal_listener;
 
     bool is_interactive = false; /// Use either interactive line editing interface or batch mode.
     bool is_multiquery = false;
